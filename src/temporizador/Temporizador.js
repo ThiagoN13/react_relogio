230a22cc8fe797c84b0eadca3ff8bf41cfab830f
JakeLee071 <guilhermejakelee@gmail.com>
2020-10-17 17:58:00 -0300

    Temporizador incompleto

--- src/temporizador/Temporizador.js
+++ src/temporizador/Temporizador.js
@@ -2,96 +2,119 @@ import React from 'react';
 import './Temporizador.css';
 
 class Temporizador extends React.Component {
-  constructor(props) {
-    super(props)
-    this.setHoras = this.setHoras.bind(this);
-    this.setsegundos = this.setsegundos.bind(this);
-    this.setminutos = this.setminutos.bind(this);
+    constructor(props) {
+      super(props)
+      this.setHoras = this.setHoras.bind(this);
+      this.setsegundos = this.setsegundos.bind(this);
+      this.setminutos = this.setminutos.bind(this);
+      
+      this.state = {
+        time: {
+          horas: 0,
+          minutos: 0,
+          segundos: 0,
+          acabou: false
+        },
+        
+        //define o tempo manualmente 
+       // soma:2 * 60 * 60 * 1000,
+       // soma: null,
+        timer: null
+        
+      };
 
-    this.state = {
-      horas: 0,
-      minutos: 0,
-      segundos: 0,
-      acabou: false
-    };
-
-  }
-  //mudança de tempo
-  setsegundos(e) {
-    this.setState({ segundos: e.target.value });
-  }
-
-  setminutos(e) {
-    this.setState({ minutos: e.target.value });
-  }
-
-  setHoras(e) {
-    this.setState({ horas: e.target.value });
-  }
-
-  pararTemporizador () {
-    clearInterval(this.myInterval);
-  }
-
-  startTemp() {
-    clearInterval(this.myInterval);
-
-    this.myInterval = setInterval(() => {
-      const { segundos = 0, minutos = 0, horas = 0 } = this.state;
-
-      if (segundos > 0) {
-        this.setState(({ segundos }) => ({
-          segundos: segundos - 1,
-        }));
-      }
-
-      if (segundos === 0 && minutos > 0) {
-        this.setState(({ minutos }) => ({
-          minutos: minutos - 1,
-          segundos: 59,
-        }));
-      }
-
-      if (segundos === 0) {
-        if (minutos === 0) {
-          if (horas === 0) {
-            this.setState({ acabou: true })
-            clearInterval(this.myInterval)
-          }
-          else {
-            this.setState(({ horas }) => ({
-              horas: horas - 1,
-              minutos: 59,
-              segundos: 59,
-            }))
-          }
-        }
-      }
-    }, 1000);
-  }
-
-  zerarTemporizador() {
-    this.setState({ horas: 0, minutos: 0, segundos: 0, acabou: false });
-    this.pararTemporizador();
-  }
-
-  alarme () {
-    const { horas, minutos, segundos, acabou } = this.state;
-
-    if (acabou && horas === 0 && minutos === 0 && segundos === 0) {
-      return (
-        <audio controls autoPlay loop>
-          <source src="/som-do-despertador.mp3" type="audio/mpeg" />
-          Your browser does not support the audio element.
-        </audio>
-      )
+      //this.startTimer = this.start.bind(this);
     }
-  }
+    
+    setsegundos(e) {
+      this.setState({ segundos: e.target.value });
+    }
+    setminutos(e) {
+      this.setState({ minutos: e.target.value });
+    }
+    setHoras(e) {
+      this.setState({ horas: e.target.value });
+    }
 
-  render() {
-    const { horas, minutos, segundos, acabou } = this.state;
+  
+    msToTime(soma) {
+    
+      let segundos = Math.floor((soma / 1000) % 60);
+      let minutos = Math.floor((soma / (1000 * 60)) % 60);
+      let horas = Math.floor((soma / (1000 * 60 * 60)) % 24);
+  
+      horas = horas.toString().padStart(2, '0');
+      minutos = minutos.toString().padStart(2, '0');
+      segundos = segundos.toString().padStart(2, '0');
+     
+  
+      return {
+        horas,
+        minutos,
+        segundos
+      
+      };
+    }
+    
+    
+    pararTemp () {
+      clearTimeout(this.timer);
+    }
+  
+   // componentDidMount() { }
 
-    return (
+    start() {
+      if (!this.state.timer) {
+        this.state.startTime = Date.now();
+        this.timer = window.setInterval(() => this.run(), 10);
+      }
+    }
+
+    zerarTemp() {
+      this.setState({ horas: 0, minutos: 0, segundos: 0});
+      this.pararTemp();
+    }
+    alarme () {
+      const { horas, minutos, segundos, acabou } = this.state.time;
+  
+      if (acabou && horas === 0 && minutos === 0 && segundos === 0) {
+        return (
+          <audio controls autoPlay loop>
+            <source src="/som-do-despertador.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        )
+      }
+    }
+    run() {
+      const { horas, minutos, segundos} = this.state.time;
+      let soma = this.state.time;
+
+      let horasMilesegundos =  horas * 1000 * 60 * 60
+      let minutosMilesegundos =  minutos* 1000 * 60
+      let segundosMilesegundos =  segundos * 1000
+
+      soma = horasMilesegundos + minutosMilesegundos + segundosMilesegundos
+
+      const diff = Date.now() - this.state.startTime;
+      
+      let remaining = this.state.soma - diff;
+      if (remaining < 0) {
+        remaining = 0;
+      }
+      this.setState(() => ({
+        time: this.msToTime(remaining)
+      }));
+      if (remaining === 0) {
+        window.clearTimeout(this.timer);
+        this.timer = null;
+      }
+    }
+
+    render() {
+      const { horas, minutos, segundos, acabou } = this.state.time;
+
+      return ( 
       <div className="Temporizador">
         <div className="col-center">
           <div className="temporizador">
@@ -101,9 +124,9 @@ class Temporizador extends React.Component {
               ) : (
                   <h5>
                     Tempo restante:<br></br>
-                    {horas < 10 ? `0${horas}` : horas}:
-                    {minutos < 10 ? `0${minutos}` : minutos}:
-                    {segundos < 10 ? `0${segundos}` : segundos}
+                    {horas < 10 ? `${horas}` : horas}:
+                    {minutos < 10 ? `${minutos}` : minutos}:
+                    {segundos < 10 ? `${segundos}` : segundos}
                   </h5>
                 )}
             </div>
@@ -113,15 +136,15 @@ class Temporizador extends React.Component {
         <div className="row">
           {this.alarme()}
         </div>
-
+        
         <div className="row">
           <div className="countdown-item">
             <input
               type="number"
               placeholder="Horas"
               min="0"
-              value={this.state.horas}
-              onChange={(event) => this.setHoras(event)}
+             value={this.state.horas}
+              onChange = {(event) => this.setHoras(event)}
             />
           </div>
 
@@ -145,32 +168,18 @@ class Temporizador extends React.Component {
             />
           </div>
         </div>
-
-        <div className="row">
-          <button
-            className="button-temp"
-            type="button"
-            onClick={(event) => this.startTemp(event)}
-          >
-            {!this.myInterval ? 'Iniciar' : 'Continuar'}
-          </button>
-          <button
-            className="button-temp"
-            type="button"
-            onClick={(event) => this.pararTemporizador(event)}
-          >
-            Parar
-          </button>
-          <button
-            className="button-temp"
-            type="button"
-            onClick={(event) => this.zerarTemporizador(event)}
-          >
-            Zerar
-          </button>
+        
+        <button onClick = {
+          this.startTimer
+        } > Iniciar </button> 
+        <button onClick ={(event) => this.pararTemp(event)
+        } > Parar </button> 
+        <button onClick ={(event) => this.zerarTemp(event)
+        } > Zerar </button> 
+        
         </div>
-      </div>
-    );
-  }
+      );
+    }
 }
-export default Temporizador;
+export default Temporizador;        
+        
